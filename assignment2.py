631873bb8a3eacc6765fbc798f7726b164f82859
zahratariq5 <zahratariq754@gmail.com>
2024-08-10 15:43:57 +0500

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,76 @@
+#Question1
+message:str = "what is your age"
+print(message.title())
+#Question2
+message:str = "i love AI"
+print(message)
+message:str = "i love pakistan"
+print(message)
+#Question3
+x:str = "Hello Zahra"
+print(f"{x} would you like to learn some python today?".title())
+#Question4
+x:str = "Hello Zahra"
+print(x.upper())
+print(x.lower())
+print(x.title())
+#Question5
+quote:str ="There are two powers in the world; one is the sword and the other is pen"
+name:str = "Quaid e Azam"
+print(f"{name} once said {quote}.title()")
+#Question6
+famous_person:str = "Quaid e Azam"
+message=f"{famous_person}There are two powers in the world; one is the sword and the other is pen "
+print(message) 
+#Question7
+x = 5
+y = 10
+z = 15
+sum = x+y+z
+print (f"the sum of x,y and z is :" , {sum})
+#Question8
+a = 5
+b = 10
+print("Before swap: ")
+print("a =",a)
+print("b =",b)
+a,b = b,a
+print("After swap: ")
+print("a =",a)
+print("b =" ,b)
+#Question9
+favouritecolour = "Black"
+print(f"favourite colour: {favouritecolour}")
+newcolour = "favouritecolour"
+print(f"New colour: {newcolour}")
+#Question10
+petname = "daisy"
+petname = "hazal"
+print(f"the new pet name: {petname}")
+#Question11
+#name = "Sunshine"
+#print(f"the variable name contain: {name}")
+#print(f"the variable 'sunshine': {Sunshine}")
+#Question12
+score:int =100
+print(f"initial score:" ,{score})
+score:int =120
+print(f"new score:" ,{score})
+#Question13
+city:str = "Sialkot"
+print(f"the city name is :{city}")
+#Question14
+text:str = "python program"
+print(f"text in title case: {text}")
+#Question15
+#mixedcase:str = "PYTHON PROGRAM"
+#print(f"text in lower case: {mixedcase.lower()}")
+#Question16
+mixedcase:str = "PYTHON PROGRAM"
+print(f"text in upper case: {mixedcase.upper()}")
+#Question17
+temperature:int =25
+print(f"the current temperature is: {temperature}degree")
+#Question18
+poem = f"in the stillness of night, stars whisper of dreams"
+print(f"the poem {poem}")
